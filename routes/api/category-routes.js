52ddc8739e950326778a9a072f4bb305d1271ff0
zanatooo <m8elzanaty@gmail.com>
2022-06-16 18:44:14 -0700

    adding the product-routes file

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,23 +6,85 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    const readerData = await Category.findAll({
+      // Add Book as a second model to JOIN with
+      include: [{ model: Product}]
+    });
+    res.status(200).json(readerData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const readerData = await Category.findByPk(req.params.id, {
+      // Add Book as a second model to JOIN with
+      include: [{ model: Product }]
+    });
+
+    if (!readerData) {
+      res.status(404).json({ message: 'No reader found with that id!' });
+      return;
+    }
+
+    res.status(200).json(readerData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const readerData = await Category.create(req.body);
+    res.status(200).json(readerData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  try {
+    const readerData = await Category.update(req.body,{
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!readerData) {
+      res.status(404).json({ message: 'No reader found with that id!' });
+      return;
+    }
+
+    res.status(200).json(readerData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  try {
+    const readerData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!readerData) {
+      res.status(404).json({ message: 'No reader found with that id!' });
+      return;
+    }
+
+    res.status(200).json(readerData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
